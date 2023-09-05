71c119dd6272277746d7f1107696cbfa6e19fda1
Pablo <112183476+pabloscopeo@users.noreply.github.com>
2023-09-05 02:46:12 -0300

    nuevo intento con varias funciones

--- tarea2/equipo.c
+++ tarea2/equipo.c
@@ -46,24 +46,83 @@ condiciones.
 #define TEAM_SIZE 5
 
 char **equipo;
-int n_jugadores;
+int n_jugadores = 0;
+char **equipo_completo;
+int equipo_completo_listo = 0;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 char **hay_equipo(char *nombre) {
     pthread_mutex_lock(&mutex);
+    printf("Jugador %s entra al mutex, se registran %d jugadores\n", nombre,
+           n_jugadores);
+    printf("equipo global: [1]  ");
+    print_equipo(equipo);
 
+    if (n_jugadores == TEAM_SIZE) {
+        printf("Jugador %s llega a un equipo completo, se resetea 'equipo' "
+               "pero no 'equipo_completo' ya que esta variable se retorna\n",
+               nombre);
+
+        equipo_completo_listo = 0;
+        reset_team(equipo);
+        n_jugadores = 0;
+        printf("equipo global: [2]  ");
+        print_equipo(equipo);
+    }
+
+    printf("Jugador %s se registra en el equipo y aumenta n_jugadores de %d a "
+           "%d\n",
+           nombre, n_jugadores, n_jugadores + 1);
     equipo[n_jugadores] = nombre;
     n_jugadores++;
+    printf("equipo global: [3]  ");
+    print_equipo(equipo);
+
+    while (n_jugadores < TEAM_SIZE || !equipo_completo_listo) {
+        printf("Jugador %s debe esperar a que se complete el equipo\n ya que "
+               "n_jugadores es %d < %d\n",
+               nombre, n_jugadores, TEAM_SIZE);
+        pthread_cond_wait(&cond, &mutex);
+    }
+    printf("Jugador %s sale del wait, n_jugadores es %d\n", nombre,
+           n_jugadores);
+    printf("equipo global: [4]  ");
+    print_equipo(equipo);
+
+    if (n_jugadores == TEAM_SIZE && !equipo_completo_listo) {
+        // Si quiere entrar el último jugador, copiar equipo en equipo_completo
+        // para que todos los jugadores lo tengan y lo retornen
+        printf("Jugador %s es el último en llegar, copia 'equipo' en "
+               "'equipo_completo' y resetea 'equipo' y 'n_jugadores' y "
+               "despierta a todos los jugadores\n",
+               nombre);
+
+        equipo_completo_listo = 1;
+        equipo_completo = copy_team(equipo);
+        // reset_team(equipo);
+        // n_jugadores = 0;
+
+        printf("equipo global: [5]  ");
+        print_equipo(equipo);
+        printf("equipo_completo global: [1]  ");
+        print_equipo(equipo_completo);
+    }
+    pthread_cond_broadcast(&cond);
+
+    printf("Jugador %s sale del mutex, n_jugadores=%d\n", nombre, n_jugadores);
+    printf("equipo global: [6]  ");
+    print_equipo(equipo);
+    printf("equipo_completo global: [2]  ");
+    print_equipo(equipo_completo);
 
     pthread_mutex_unlock(&mutex);
-    return equipo;
+    return equipo_completo;
 }
 
 void init_equipo(void) {
     equipo = (char **)malloc(TEAM_SIZE * sizeof(char *));
-    n_jugadores = 0;
 }
 
 void end_equipo(void) {
@@ -75,4 +134,46 @@ void print_equipo(char **equipo) {
         printf("%s ", equipo[i]);
     }
     printf("\n");
+}
+
+void print_state(char *nombre, char *message) {
+    printf("Jugador %s: %s\n n_jugadores=%d\n equipo_completo_listo=%d\n "
+           "equipo global: ",
+           nombre, message, n_jugadores, equipo_completo_listo);
+    print_equipo(equipo);
+    printf("equipo_completo global: ");
+    print_equipo(equipo_completo);
+    printf("\n");
+}
+
+int full_team(char **equipo) {
+    for (int i = 0; i < TEAM_SIZE; i++) {
+        if (equipo[i] == NULL) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int empty_team(char **equipo) {
+    for (int i = 0; i < TEAM_SIZE; i++) {
+        if (equipo[i] != NULL) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void reset_team(char **equipo) {
+    for (int i = 0; i < TEAM_SIZE; i++) {
+        equipo[i] = NULL;
+    }
+}
+
+char **copy_team(char **equipo) {
+    char **new_team = (char **)malloc(TEAM_SIZE * sizeof(char *));
+    for (int i = 0; i < TEAM_SIZE; i++) {
+        new_team[i] = equipo[i];
+    }
+    return new_team;
 }
--- tarea2/equipo.h
+++ tarea2/equipo.h
@@ -2,3 +2,8 @@ char **hay_equipo(char *nombre);
 void init_equipo(void);
 void end_equipo(void);
 void print_equipo(char **equipo);
+void print_state(char *nombre, char *message);
+int full_team(char **equipo);
+int empty_team(char **equipo);
+void reset_team(char **equipo);
+char **copy_team(char **equipo);
